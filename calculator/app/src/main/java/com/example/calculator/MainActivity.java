ebf63bc56dc2530cd2bdc5ec3d52a536d6c5b7ba
liljack13 <nptrung031201@gmail.com>
2021-11-06 21:39:31 +0700

    cal

--- calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,271 +3,269 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.SpannableStringBuilder;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
+import android.view.View;
 
 
 
 public class MainActivity extends AppCompatActivity {
-    String curr, res ;
-    Boolean dot_status,op_status;
-
-    private EditText calculation,result;
-    private Button btn0;
-    private Button btn1;
-    private Button btn2;
-    private Button btn3;
-    private Button btn4;
-    private Button btn5;
-    private Button btn6;
-    private Button btn7;
-    private Button btn8;
-    private Button btn9;
-    private Button btnMul;
-    private Button btnDiv;
-    private Button btnPlus;
-    private Button btnMinus;
-    private Button btnDot;
-    private ImageButton btnDel;
-    private Button btnClear;
-    private Button btnCE;
-    private Button btnEqual;
+    String input1,input2,output,res,fixed_output;
+    boolean flag;
+    private EditText result,calculation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        calculation =(EditText) findViewById(R.id.calculation);
-        result =(EditText) findViewById(R.id.result);
+        calculation = findViewById(R.id.calculation);
+        result = findViewById(R.id.result);
 
-        curr ="";
-        res = "";
-        dot_status = false;
-        op_status = false;
+        input1 ="";
+        input2 ="";
+        res  ="";
+        output="";
+        fixed_output = "";
+        flag = true;
 
-        btn0 =(Button) findViewById(R.id.Zero);
-        btn1 =(Button) findViewById(R.id.One);
-        btn2 =(Button) findViewById(R.id.Two);
-        btn3 =(Button) findViewById(R.id.Three);
-        btn4 =(Button) findViewById(R.id.Four);
-        btn5 =(Button) findViewById(R.id.Five);
-        btn6 =(Button) findViewById(R.id.Six);
-        btn7 =(Button) findViewById(R.id.Seven);
-        btn8 =(Button) findViewById(R.id.Eight);
-        btn9 =(Button) findViewById(R.id.Nine);
-        btnMul =(Button) findViewById(R.id.MultiBTN);
-        btnDiv =(Button) findViewById(R.id.DivBTN);
-        btnPlus =(Button) findViewById(R.id.PlusBTN);
-        btnMinus =(Button) findViewById(R.id.MinusBTN);
-        btnDot =(Button) findViewById(R.id.Dot);
-        btnDel = (ImageButton) findViewById(R.id.back_space);
-        btnClear =(Button) findViewById(R.id.Clear);
-        btnCE =(Button) findViewById(R.id.Clear_EntryBTN);
-        btnEqual = (Button) findViewById(R.id.Equal);
+        Button btn0 = findViewById(R.id.Zero);
+        Button btn1 = findViewById(R.id.One);
+        Button btn2 = findViewById(R.id.Two);
+        Button btn3 = findViewById(R.id.Three);
+        Button btn4 = findViewById(R.id.Four);
+        Button btn5 = findViewById(R.id.Five);
+        Button btn6 = findViewById(R.id.Six);
+        Button btn7 = findViewById(R.id.Seven);
+        Button btn8 = findViewById(R.id.Eight);
+        Button btn9 = findViewById(R.id.Nine);
+        Button btnMul = findViewById(R.id.MultiBTN);
+        Button btnDiv = findViewById(R.id.DivBTN);
+        Button btnPlus = findViewById(R.id.PlusBTN);
+        Button btnMinus = findViewById(R.id.MinusBTN);
+        Button btnDot = findViewById(R.id.Dot);
+        ImageButton btnDel = findViewById(R.id.back_space);
+        Button btnClear = findViewById(R.id.Clear);
+        Button btnCE = findViewById(R.id.Clear_EntryBTN);
+        Button btnEqual = findViewById(R.id.Equal);
+        Button btnChaOp = findViewById(R.id.changeOp);
 
-        btn0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                curr = curr + "0";
-                DisplayOne(curr);
-            }
-        });
 
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                curr = curr + "1";
-                DisplayOne(curr);
-            }
-        });
+    }
 
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                curr = curr + "2";
-                DisplayOne(curr);
-            }
-        });
-
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                curr = curr + "3";
-                DisplayOne(curr);
-            }
-        });
-
-        btn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                curr = curr + "4";
-                DisplayOne(curr);
-            }
-        });
-
-        btn5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                curr = curr + "5";
-                DisplayOne(curr);
-            }
-        });
-
-        btn6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                curr = curr + "6";
-                DisplayOne(curr);
-            }
-        });
-
-        btn7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                curr = curr + "7";
-                DisplayOne(curr);
-            }
-        });
-
-        btn8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                curr = curr + "8";
-                DisplayOne(curr);
-            }
-        });
-
-        btn9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                curr = curr + "9";
-                DisplayOne(curr);
-            }
-        });
-
-        btnDot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(!dot_status){
-                    curr = curr + ".";
-                    dot_status = true;
-                    DisplayOne(curr);
+    public void onButtonClicked(View view) {
+        Button button = (Button) view;
+        String data = button.getText().toString();
+        switch (data) {
+            case "C":
+                input1 = "";
+                input2 = "";
+                output = "";
+                fixed_output = "";
+                result.setText(output);
+                flag = true;
+                calculation.setText("");
+                result.setText("");
+                break;
+            case "CE":
+                if (flag) {
+                    input1 = "";
+                    result.setText("");
+                } else {
+                    input2 = "";
+                    result.setText("");
                 }
-            }
-        });
-
-        btnMinus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(curr.charAt(curr.length() - 1) == '.'){
-                    curr = curr.substring(0,curr.length()-1);
+                break;
+            case "%":
+                double s = Double.parseDouble(result.getText().toString()) / 100;
+                if (flag) {
+                    input1 = Double.toString(s);
+                    calculation.setText(input1);
+                } else {
+                    input2 = Double.toString(s);
+                    calculation.setText(input2);
                 }
-                if (!op_status){
-                    curr = curr + " - ";
-                    DisplayOne(curr);
-                    op_status = true;
+                break;
+            case "^":
+                solve();
+                if (flag) {
+                    input1 += "^";
+                    calculation.setText(input1);
+                    flag = false;
+                } else {
+                    input2 += "^";
+                    calculation.setText(input2);
+                    flag = true;
                 }
-            }
-        });
-
-        btnPlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(curr.charAt(curr.length() - 1) == '.'){
-                    curr = curr.substring(0,curr.length()-1);
-                }
-                if (!op_status){
-                    curr = curr + " + ";
-                    DisplayOne(curr);
-                    op_status = true;
-                }
-            }
-        });
-
-        btnDiv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(curr.charAt(curr.length() - 1) == '.'){
-                    curr = curr.substring(0,curr.length()-1);
-                }
-                if (!op_status){
-                    curr = curr + " / ";
-                    DisplayOne(curr);
-                    op_status = true;
-                }
-            }
-        });
-
-        btnMul.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(curr.charAt(curr.length() - 1) == '.'){
-                    curr = curr.substring(0,curr.length()-1);
-                }
-                if (!op_status){
-                    curr = curr + " * ";
-                    DisplayOne(curr);
-                    op_status = true;
-                }
-            }
-        });
-
-        btnDel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                backSpace();
-                DisplayOne(curr);
-            }
-        });
-
-        btnEqual.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(op_status && curr.charAt(curr.length()-1) != ' '){
-                    String[] Tokens = curr.split(" ");
-                    switch (Tokens[1].charAt(0)){
-                        case '+':
-                            res = Double.toString(Double.parseDouble(Tokens[0]) + Double.parseDouble(Tokens[2]));
-                            break;
-                        case '-':
-                            res = Double.toString(Double.parseDouble(Tokens[0]) - Double.parseDouble(Tokens[2]));
-                            break;
-                        case '*':
-                            res = Double.toString(Double.parseDouble(Tokens[0]) * Double.parseDouble(Tokens[2]));
-                            break;
-                        case '/':
-                            res = Double.toString(Double.parseDouble(Tokens[0]) / Double.parseDouble(Tokens[2]));
-                            break;
+                break;
+            case "=":
+                solve();
+                break;
+            case "cawn":
+                break;
+            case "1/x":
+                break;
+            default:
+                boolean b = data.equals("+") || data.equals("*") || data.equals("/") || data.equals("-");
+                if (flag) {
+                    if (input1 == null) input1 = "";
+                    if (b) {
+                        solve();
+                        input1 += data;
+                        calculation.setText(input1);
+                        flag = false;
+                        input2 = "";
+                        result.setText(input1.substring(0,input1.length()-1));
+                    }else {
+                        input1 += data;
+                        result.setText(input1);
                     }
-                    DisplayTwo(res);
+                } else {
+                    if (input2 == null) input2 = "";
+                    if (b) {
+                        solve();
+                        input2 += data;
+                        calculation.setText(input2);
+                        flag = true;
+                        input1 = "";
+                        result.setText(input2.substring(0,input2.length()-1));
+                    }else {
+                        input2 += data;
+                        result.setText(input2);
+                    }
                 }
-            }
-        });
-    }
-
-    public void DisplayOne(String s){
-        calculation.setText(s);
-    }
-
-    public void DisplayTwo(String s){
-        result.setText(s);
-    }
-
-    public void backSpace(){
-        if(!curr.isEmpty()){
-            if(curr.charAt(curr.length()-1) == '.'){
-                dot_status = false;
-            }
-            if(curr.charAt(curr.length()-1) == ' '){
-                op_status = false;
-                curr=curr.substring(0,curr.length()-3);
-            }else curr = curr.substring(0,curr.length()-1);
         }
     }
-}
+
+    private void solve() {
+        if (input1.contains("+")||input2.contains("+")) {
+            try {
+                double d;
+                if(input1.charAt(input1.length()-1) == '+') {
+                    d = Double.parseDouble(input1.substring(0,input1.length()-1)) + Double.parseDouble(input2);
+                }else{
+                    d = Double.parseDouble(input1) + Double.parseDouble(input2.substring(0,input2.length()-1));
+                }
+                output = Double.toString(d);
+                fixed_output = cutDecimal(output);
+                result.setText(fixed_output);
+                if(flag) {
+                    input1 = d + "";
+                }else{
+                    input2 = d +"";
+                }
+            } catch (Exception e) {
+                result.setText(e.getMessage());
+            }
+        }
+
+        if (input1.contains("-")||input2.contains("-")) {
+            try {
+                double d;
+                if(input1.charAt(input1.length()-1) == '-') {
+                    d = Double.parseDouble(input1.substring(0,input1.length()-1)) - Double.parseDouble(input2);
+                }else{
+                    d = Double.parseDouble(input2.substring(0,input2.length()-1)) - Double.parseDouble(input1);
+                }
+                output = Double.toString(d);
+                fixed_output = cutDecimal(output);
+                result.setText(fixed_output);
+                if(flag) {
+                    input1 = d + "";
+                }else{
+                    input2 = d +"";
+                }
+            } catch (Exception e) {
+                result.setText(e.getMessage());
+            }
+        }
+
+        if (input1.contains("*")||input2.contains("*")) {
+            try {
+                double d;
+                if(input1.charAt(input1.length()-1) == '+') {
+                    d = Double.parseDouble(input1.substring(0,input1.length()-1)) * Double.parseDouble(input2);
+                }else{
+                    d = Double.parseDouble(input1) * Double.parseDouble(input2.substring(0,input2.length()-1));
+                }
+                output = Double.toString(d);
+                fixed_output = cutDecimal(output);
+                result.setText(fixed_output);
+                if(flag) {
+                    input1 = d + "";
+                }else{
+                    input2 = d +"";
+                }
+            } catch (Exception e) {
+                result.setText(e.getMessage());
+            }
+        }
+
+        if (input1.contains("/")||input2.contains("/")) {
+            try {
+                double d;
+                if(input1.charAt(input1.length()-1) == '/') {
+                    d = Double.parseDouble(input1.substring(0,input1.length()-1)) / Double.parseDouble(input2);
+                }else{
+                    d = Double.parseDouble(input2.substring(0,input2.length()-1)) / Double.parseDouble(input1);
+                }
+                output = Double.toString(d);
+                fixed_output = cutDecimal(output);
+                result.setText(fixed_output);
+                if(flag) {
+                    input1 = d + "";
+                }else{
+                    input2 = d +"";
+                }
+            } catch (Exception e) {
+                result.setText(e.getMessage());
+            }
+        }
+
+        if (input1.contains("^")||input2.contains("^")) {
+            try {
+                double d;
+                if(input1.charAt(input1.length()-1) == '^') {
+                    d = Math.pow(Double.parseDouble(input1.substring(0,input1.length()-1)),Double.parseDouble(input2));
+                }else{
+                    d = Math.pow(Double.parseDouble(input1),Double.parseDouble(input2.substring(0,input2.length()-1)));
+                }
+                output = Double.toString(d);
+                fixed_output = cutDecimal(output);
+                result.setText(fixed_output);
+                if(flag) {
+                    input1 = d + "";
+                }else{
+                    input2 = d +"";
+                }
+            } catch (Exception e) {
+                result.setText(e.getMessage());
+            }
+        }
+    }
+
+    public void backSpace(View view) {
+        if (flag) {
+            if (!input1.isEmpty()) {
+                input1 = input1.substring(0, input1.length() - 1);
+            }
+            result.setText(input1);
+        }else{
+            if (!input2.isEmpty()) {
+                input2 = input2.substring(0, input2.length() - 1);
+            }
+            result.setText(input2);
+        }
+    }
+
+    private String cutDecimal(String number) {
+        String[] n = number.split("\\.");
+        if(n.length > 1) {
+            if (n[1].equals("0")) {
+                number = n[0];
+            }
+        }
+        return number;
+    }
+    }
